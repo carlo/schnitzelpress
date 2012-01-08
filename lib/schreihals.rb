67c4fa02baac22551d8fcaec44110c2b6ef3058e
Hendrik Mans <hendrik@mans.de>
2012-01-08 14:53:56 +0100

    Move app actions into their own mixin.

--- lib/schreihals.rb
+++ lib/schreihals.rb
@@ -11,11 +11,13 @@ require 'rack/codehighlighter'
 
 require 'active_support/core_ext/string/inflections'
 require 'active_support/core_ext/class'
+require 'active_support/concern'
 
 require 'schreihals/static'
 require 'schreihals/helpers'
 require 'schreihals/document'
 require 'schreihals/post'
+require 'schreihals/actions'
 require 'schreihals/app'
 
 module Schreihals
--- lib/schreihals/app.rb
+++ lib/schreihals/app.rb
@@ -9,6 +9,7 @@ module Schreihals
     use Rack::Codehighlighter, :coderay, :markdown => true, :element => "pre>code", :pattern => /\A:::(\w+)\s*\n/
 
     helpers Schreihals::Helpers
+    include Schreihals::Actions
 
     configure do
       set :blog_title, "My Schreihals Blog"
@@ -40,44 +41,6 @@ module Schreihals
       else
         raise "Unknown documents store '#{settings.documents_store}'."
       end
-    end
-
-    before do
-      refresh_documents! if refresh_documents_now?
-      cache_control :public, :must_revalidate, :max_age => 60
-    end
-
-    get '/' do
-      @posts = latest_posts
-
-      @show_description = true
-      haml :index
-    end
-
-    get '/schreihals.css' do
-      scss :schreihals
-    end
-
-    get '/atom.xml' do
-      @posts = latest_posts
-
-      xml = haml :atom, :layout => false
-
-      doc = Nokogiri::XML(xml)
-      doc.css("content img").each do |node|
-        node['src'] = absolutionize(node['src'])
-      end
-
-      content_type 'application/xml+atom'
-      doc.to_xml
-    end
-
-    get '/:year/:month/:day/:slug/?' do |year, month, day, slug|
-      render_page(slug)
-    end
-
-    get '/:slug/?' do |slug|
-      render_page(slug)
     end
 
     def render_page(slug)
